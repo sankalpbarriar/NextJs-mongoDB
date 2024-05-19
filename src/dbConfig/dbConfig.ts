b5628254b6eae8bc45a6627b52af0da75d5336cf
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2024-05-19 08:57:46 +0530

    testing connection error after deployment

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();  // Load environment variables from .env file
+dotenv.config();
 
 export async function connect() {
   try {
@@ -9,7 +9,9 @@ export async function connect() {
     if (!uri) {
       throw new Error('MONGO_URI environment variable not defined');
     }
-    
+
+    console.log('Connecting to MongoDB URI:', uri.split('@')[1]); // Debug: Don't log the full URI in production
+
     await mongoose.connect(uri);
     const connection = mongoose.connection;
 
@@ -19,11 +21,16 @@ export async function connect() {
     });
 
     connection.on('error', (err) => {
-      console.log('MongoDB connection error, please make sure DB is up and running: ' + err);
+      console.error('MongoDB connection error, please make sure DB is up and running: ' + err);
       process.exit(1);
     });
+
+    connection.on('disconnected', () => {
+      console.log('MongoDB disconnected');
+    });
+
   } catch (error) {
-    console.log('Something went wrong in connecting to DB:');
-    console.log(error);
+    console.error('Something went wrong in connecting to DB:');
+    console.error(error);
   }
 }
